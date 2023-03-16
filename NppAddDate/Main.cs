4bfe0a78efe74acffccb9e9dc9f07520d7eccf17
dimmik <ad@dimmik.org>
2023-03-16 08:06:26 +0100

    some configuration (date format and extension to work with)

--- NppAddDate/Main.cs
+++ NppAddDate/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Kbg.NppPluginNET.PluginInfrastructure;
@@ -12,26 +13,37 @@ namespace Kbg.NppPluginNET
         static string iniFilePath = null;
 
         static bool DoInsertDate = true;
+        static string DatetimeFmt = "yyyy.MM.dd HH:mm:ss";
+        static string ApplicableExtension = ".wlog";
+        static char ToggleAddDateChar = '~';
+        static char AddDateKey = (char)10; // LF
 
         public static void OnNotification(ScNotification notification)
         {  
             if (notification.Header.Code == (uint)SciMsg.SCI_ADDTEXT)
             {
-                if (notification.Mmodifiers == 126) // "~" - toggle adding the date 
+                if (notification.Mmodifiers == ToggleAddDateChar) // toggle adding the date 
                 {
                     DoInsertDate = !DoInsertDate;
                 } 
-                if (notification.Mmodifiers == 10) // LF
+                if (notification.Mmodifiers == AddDateKey) 
                 {
                     if (DoInsertDate)
                     {
                         var npp = new NotepadPPGateway();
                         var path = npp.GetCurrentFilePath();
-                        if (path.EndsWith(".wlog"))
+                        if (path.EndsWith(ApplicableExtension))
                         {
                             var scih = PluginBase.GetCurrentScintilla();
                             ScintillaGateway sci = new ScintillaGateway(scih);
-                            var now = DateTime.Now.ToString("yyyy.MM.dd HH:mm:ss") + " ";
+                            string now;
+                            try
+                            {
+                                now = DateTime.Now.ToString(DatetimeFmt) + " ";
+                            } catch
+                            {
+                                now = DateTime.Now.ToString() + " ";
+                            }
                             sci.AddText(now.Length, now);
                         }
                     }
@@ -55,8 +67,9 @@ namespace Kbg.NppPluginNET
             if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
             iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
             //someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+            ReadConfig();
 
-            PluginBase.SetCommand(0, "Info", ShowInfo, new ShortcutKey(false, false, false, Keys.None));
+            PluginBase.SetCommand(0, "Info and Reload Config", ShowInfoAndReloadConfig, new ShortcutKey(false, false, false, Keys.None));
         }
 
         internal static void PluginCleanUp()
@@ -64,12 +77,52 @@ namespace Kbg.NppPluginNET
             //Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
         }
 
-        internal static void ShowInfo()
+        internal static void ShowInfoAndReloadConfig()
         {
-            MessageBox.Show(@"On each newline (LF) in files with extension .wlog adds a date in format yyyy.MM.dd HH:mm:ss at the line start
+            ReadConfig();
+            MessageBox.Show($@"On each newline (LF) in files with extension {ApplicableExtension} adds a date in format {DatetimeFmt} at the line start
 Tilda ('~') toggles adding date.
 On startup 'add date' is enabled
+Config file: '{Path.GetFullPath(iniFilePath)}'
+Is Date currently being added? {DoInsertDate}
 ");
         }
+
+        static void ReadConfig()
+        {
+            try
+            {
+                var iniContent = File.ReadAllLines(iniFilePath);
+                var extLine = iniContent.Where(l => l.Trim().StartsWith("ApplicableExtension=")).FirstOrDefault();
+                if (extLine != null)
+                {
+                    var ext = extLine.Trim().Substring("ApplicableExtension=".Length).Trim();
+                    if (!string.IsNullOrWhiteSpace(ext))
+                    {
+                        ApplicableExtension= ext;
+                    }
+                }
+                var fmtLine = iniContent.Where(l => l.Trim().StartsWith("DateTimeFmt=")).FirstOrDefault();
+                if (fmtLine != null)
+                {
+                    var fmt = fmtLine.Trim().Substring("DateTimeFmt=".Length).TrimStart();
+                    if (!string.IsNullOrWhiteSpace(fmt))
+                    {
+                        try
+                        {
+                            var formatted = DateTime.Now.ToString(fmt); // if it is successful
+                            DatetimeFmt = fmt;
+                        } catch
+                        {
+                            // nothing
+                        }
+                    }
+                }
+            } catch
+            {
+                // nothing
+            }
+        }
+
     }
 }
